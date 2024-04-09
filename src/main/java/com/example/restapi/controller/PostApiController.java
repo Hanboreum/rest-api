d46e5f9ffe21178056dec23e7cb2759cf36f70ac
fullm <matoma1_@naver.com>
2024-04-09 19:55:01 +0900

    POST 실습 사용자 정보를 받음

--- src/main/java/com/example/restapi/controller/PostApiController.java
+++ src/main/java/com/example/restapi/controller/PostApiController.java
@@ -1,6 +1,7 @@
 package com.example.restapi.controller;
 
 import com.example.restapi.model.BookRequest;
+import com.example.restapi.model.userInfo;
 import org.springframework.web.bind.annotation.*;
 
 @RestController //restapi 방식 지원
@@ -10,6 +11,13 @@ public class PostApiController {
     public String post( @RequestBody BookRequest bookRequest){
         System.out.println(bookRequest);
         return bookRequest.toString(); //문자열로 반환
+    }
+
+    //http://localhost:8080/api/post2
+    @PostMapping("/post2")
+    public String user(@RequestBody userInfo userInfo){
+        System.out.println(userInfo);
+        return userInfo.toString();
     }
 }
 /*

31c8653c6c5ce6eeda208e496db40ed6e4508c2e
fullm <matoma1_@naver.com>
2024-04-09 19:37:36 +0900

    POST 화면 출력 위해 return toString

--- src/main/java/com/example/restapi/controller/PostApiController.java
+++ src/main/java/com/example/restapi/controller/PostApiController.java
@@ -1,17 +1,22 @@
 package com.example.restapi.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.restapi.model.BookRequest;
+import org.springframework.web.bind.annotation.*;
 
 @RestController //restapi 방식 지원
 @RequestMapping("/api") //어떤 주소를 맵핑할건지
 public class PostApiController {
-
-    @PostMapping("/post") //post의 요청에 대한 요청을 받음
-    public void post(
-
-    ){
-
+    @PostMapping("/post")
+    public String post( @RequestBody BookRequest bookRequest){
+        System.out.println(bookRequest);
+        return bookRequest.toString(); //문자열로 반환
     }
 }
+/*
+ //http://localhost:8080/api/post
+    @PostMapping("/post") //post의 요청에 대한 요청을 받음
+    public BookRequest post(@RequestBody BookRequest bookRequest){
+        System.out.println(bookRequest);
+        return bookRequest;
+    }
+ */

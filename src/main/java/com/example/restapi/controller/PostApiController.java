67ca43e4062311d57633667e1cb371bf725aacbd
fullm <matoma1_@naver.com>
2024-04-09 18:35:00 +0900

    POST 방식 초기 세팅

--- src/main/java/com/example/restapi/controller/PostApiController.java
+++ src/main/java/com/example/restapi/controller/PostApiController.java
@@ -1,7 +1,17 @@
 package com.example.restapi.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController //restapi 방식 지원
+@RequestMapping("/api") //어떤 주소를 맵핑할건지
 public class PostApiController {
+
+    @PostMapping("/post") //post의 요청에 대한 요청을 받음
+    public void post(
+
+    ){
+
+    }
 }

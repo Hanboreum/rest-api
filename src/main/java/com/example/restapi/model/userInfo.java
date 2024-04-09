8db27fa58d837e3ed004f7837ed9dd02c1d30e23
fullm <matoma1_@naver.com>
2024-04-09 20:33:49 +0900

    @JsonNaming 추가

--- src/main/java/com/example/restapi/model/userInfo.java
+++ src/main/java/com/example/restapi/model/userInfo.java
@@ -1,12 +1,16 @@
 package com.example.restapi.model;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.core.convert.Property;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class userInfo {
     private String name;
     private Integer tel; //제이슨 활용시 래퍼런스 타입 추천

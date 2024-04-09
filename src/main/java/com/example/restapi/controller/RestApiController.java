090dec5d2809045ad22d268e50ee81f4a9f30091
fullm <matoma1_@naver.com>
2024-04-09 18:29:25 +0900

    POST 방식

--- src/main/java/com/example/restapi/controller/RestApiController.java
+++ src/main/java/com/example/restapi/controller/RestApiController.java
@@ -1,4 +1,4 @@
-package com.example.restapi;
+package com.example.restapi.controller;
 
 import com.example.restapi.model.BookQueryParam;
 import org.springframework.web.bind.annotation.*;
@@ -18,7 +18,7 @@ public class RestApiController {
     //http://localhost:8080/api/echo/hello/age/24/is-man/false
     @GetMapping(path ="/echo/{message}/age/{age}/is-man/{isMan}") //Path variable 있다면 중괄호가 들어감
     public String echo(@PathVariable(name = "message") String msg,//주소에는 대문자를 넣지 않아서 _ 사용
-                       @PathVariable int age,
+                       @PathVariable int age, //PathVariable: 경로에서 값을 추출할 때 사용.
                        @PathVariable boolean isMan){
         System.out.println("echo message" + msg);
         System.out.println("age" + age);
@@ -34,7 +34,7 @@ public class RestApiController {
     // http://localhost:8080/api/book?category=IT&issuedYear=2023&issued-month=01&issued_day=31
     //category 부터 파싱. category 는 String 이겠죠
     @GetMapping(path = "/book")
-    public void queryParam( @RequestParam String category,
+    public void queryParam( @RequestParam String category, // RequestParam: 쿼리 매개변수에서 값을 추출할 때 사용.
                             @RequestParam String issuedYear,
                             @RequestParam (name = "issued-month")String issuedMonth, //카멜 케이스 맞춰주려고
                             @RequestParam (name ="issued_day")String issuedDay){
@@ -55,6 +55,7 @@ public class RestApiController {
 /*
 //TODO int 형태의 param 2가지를 받아서 두 수의 합, 곱셈 해주는 주소 설계.
     // TODO String, boolean
+
     // http://localhost:8080/plus/mul
     @GetMapping( path = "/plus/mul")
     public void num(@PathVariable int plus , @PathVariable int mul){
@@ -63,7 +64,18 @@ public class RestApiController {
         System.out.println(plus + n+m);
         System.out.println(mul + n*m);
     }
+     //http://localhost:8080/api/book3?category=IT&issuedYear=2023&issuedMonth=01&issuedDay=31
+    @GetMapping (path = "/book3")
+    public String query23( @RequestParam String category, @RequestParam String issuedYear, @RequestParam String issuedMonth , @RequestParam String issuedDay ){
+        return "Category: " + category + ", Issued Year: " + issuedYear + ", Issued Month: " + issuedMonth + ", Issued Day: " + issuedDay;
+        //리턴시 화면에 나타남.
+    }
 
+    http://localhost:8080/api/book4?category=IT&issuedYear=2023&issuedMonth=01&issuedDay=31
+@GetMapping(path = "/book4")
+    public void query4 ( BookQueryParam bookQueryParam){
+        System.out.println(bookQueryParam);
+    }
 ----20240408
  @GetMapping(path = "/level/{level}")
     public int level(@PathVariable int level){

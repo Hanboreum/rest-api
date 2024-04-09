97690b883ce972919b068507055fb46c7b2b81b9
fullm <matoma1_@naver.com>
2024-04-09 20:38:05 +0900

    Fix :반환 타입 변경

--- src/main/java/com/example/restapi/model/UserInfo.java
+++ src/main/java/com/example/restapi/model/UserInfo.java
@@ -5,13 +5,12 @@ import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.core.convert.Property;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
-public class userInfo {
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)//해당 클래스는 스네이크 케이스를 받겠다.
+public class UserInfo {
     private String name;
     private Integer tel; //제이슨 활용시 래퍼런스 타입 추천
     private String email;

1a12548cd92049a03ef347c01acfc75e20b95187
fullm <matoma1_@naver.com>
2024-04-11 15:56:40 +0900

    String 으로 데이터 리턴

--- src/main/java/com/example/restapi/controller/ResponseApiController.java
+++ src/main/java/com/example/restapi/controller/ResponseApiController.java
@@ -1,21 +1,25 @@
 package com.example.restapi.controller;
 
 import com.example.restapi.model.UserInfo;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1")
 public class ResponseApiController {
 
     //http://localhost:8080/api/v1
     @GetMapping("") //default address
-    public UserInfo user(){
+    public String user(){
         var user = new UserInfo();
         user.setUserName("nameuser");
         user.setUserEmail("@gmail.com");
         user.setUserTel(0234543);
-        return user;
+
+        log.info("user: {}", user);
+        return user.toString();
     }
 }
